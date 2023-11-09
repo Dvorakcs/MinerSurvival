26f782c1b986093d77f84c7942159eed81659142
Marcelo <mshf15@hotmail.com>
2023-11-09 19:15:16 -0300

    #7 - adicionando o mongo db e criando rotas de log

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,7 @@
-const app = require('./app')
-const appWs = require('./app-ws')
+const app = require('./services/app')
+const appWs = require('./services/app-ws')
 
 const server  = app.listen(process.env.PORT || 3000, () => {
     console.log('app Express ins Running!')
 })
-
 appWs(server);
--- Server/services/app.js
+++ Server/services/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const db = require('../context/db')
 const app = express();
 
 app.use(cors({origin: process.env.CORS_ORIGIN || '*'}));
@@ -13,8 +14,13 @@ app.use(express.json());
 
 app.use(morgan('dev'));
 
-app.post('/login', (req,res,next) => {
-    res.json({token: ' 123456'});
-});
+app.get('/login',async (req,res,next) => {
+    try {
+        const docs = await db.findAll();
+        console.log(docs)
+    } catch (error) {
+            next(error)
+    }
+}) 
 
 module.exports = app
